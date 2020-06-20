12f4a2e423d85555ab84728945c87745c4b8f101
Piotr Kluz <piox89@gmail.com>
2020-06-20 14:41:59 +0200

    Temporary disable typescript plugin. Official plugin crashes every time when code change to not compilable state.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,5 +20,5 @@ const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
+  // on('file:preprocessor', cypressTypeScriptPreprocessor)
 }
