8503cb674427b3948ee6ebbd208422fcaf49af49
Piotr Kluz <piox89@gmail.com>
2020-06-20 14:42:18 +0200

    Add simple tests for browse events

--- cypress/integration/coursedog/browse.events.spec.js
+++ cypress/integration/coursedog/browse.events.spec.js
@@ -1,31 +1,53 @@
 /// <reference types="cypress" />
 
+const { todayIs, openTab, waitForReload } = require("./util")
 
 context('Browse events', () => {
     beforeEach(() => {
-    //   cy.visit('https://damian-events.coursedog.com/')
-    })
-
-    it('Empty today events', () => {
         cy.visit("https://damian-events.coursedog.com")
-        
-        
-         
-        // now is 16th
-
-        //today events is empty
     })
 
-    it('Today events shows list', () => {
-        // now is 18th
+    it('should display notification "No events today"', () => {
+        todayIs("2020-06-16")
+        openTab("today")
 
-        //today events contain 1 event
-        //featured events contain 3 events
+        cy.get('#main-content .flex h1').should("contain.text", "No events today")
     })
 
-    it('Event information', () => {
+    it('should display today events', () => {
+        todayIs("2020-06-18")
+        openTab("today")
+
+        cy.get("div[role=group] .card").should("have.length.greaterThan", 0)
+        // todo date assertion
+    })
+
+    it('should display featured events', () => {
+        todayIs("2020-06-18")
+        openTab("featured")
+
+        cy.get("div[role=group] .card").should("have.length.greaterThan", 0)
+        //todo date assertion
+    })
+
+    it('should display event information', () => {
         //given
         // Open event "QA Submission Task"
+        // openEvent("QA Task Submission")
+        openTab("featured")
+        cy.contains("QA Task Submission").click()
+        waitForReload()
+
+        cy.get("article button").contains("Add to calendar").should("be.visible")
+        cy.get("article button").contains("Add to Google Calendar").should("be.visible")
+        cy.get('article svg[data-icon="map-marker-alt"]').next() //Address
+            .should("not.be.empty")
+
+        cy.get("label").contains("Event Type").next().should("not.be.empty")
+        cy.get("label").contains("Organized by").next().should("not.be.empty")
+
+        cy.get("aside > div[aria-label^='Event card for']")
+            .should("have.length.greaterThan", 0)
 
 
         //expect
@@ -40,15 +62,28 @@ context('Browse events', () => {
     })
 
     it('Search for events', () => {
-        // type "Finae in search box"
-        //confirm
+        // type "Finale in search box"
+        cy.get("form[role=search] > input").type("Finale{enter}")
 
         // expect 1 matching event
+        cy.get("#search-results a[aria-label^='Event name']")
+            .should("have.length", 1)
+            .should("contain.text", "QA Recruitment Finale")
     })
 
     it('Fiter by organization', () => {
         //when
         // Select organization "Coursedog Team"
+        todayIs("2020-06-18")
+        cy.get("#orgSelect").select("Coursedog Team")
+        waitForReload()
+
+
+        //then
+        cy.get("#search-results a[aria-label^='Event name']").should("have.length", 5)
+        cy.get("#search-results").contains("Organized by").each((el, i, list) => {
+            expect(el.next().text()).contain("Coursedog Team")
+        })
 
         //then
         // there should be 5 events in result
@@ -57,7 +92,11 @@ context('Browse events', () => {
 
     it('pick date from calendar', () => {
         // pick 18 
+        cy.get(".vc-w-full .vc-day-content").contains(/^18$/) //trivial solution, works only for current month
+            .click()
+        waitForReload()
 
+        cy.get("div[role=group] .card").should("have.length", 1)
         // see that 1 found event
         // and event date is 18
     })
@@ -66,3 +105,5 @@ context('Browse events', () => {
 
 //BUGS: 
 // Language: Calendar is PL, others are EN
+// server side render on main page return empty featured events
+// Select by "Coursedog Team" don't show 
---
+++ cypress/integration/coursedog/util.js
@@ -0,0 +1,36 @@
+const moment = require('moment')
+
+/**
+ * @param {string} date
+ * @example todayIs("2020-01-01") 
+ */
+export function todayIs(date) {
+    const dateObj = new Date(date)
+    if(!dateObj.toString() == "Invalid Date") `Date should have format YYYY-MM-DD, actual value: ${date} not match.`
+    
+    cy.log(`Today is ${date}`)
+    cy.clock(dateObj.valueOf())
+}
+
+/**
+ * Opens tab by click on link.
+ * 
+ * @param {"featured" | "today" | "upcoming"} tabName 
+ * @example openTab("featured")
+ */
+export function openTab(tabName) {
+    cy.get(`a[href="/${tabName}"]`).click()
+    waitForReload(20000)
+}
+
+/**
+ * Wait for reload page content.
+ * @param {number} timeLimitMs 
+ */
+export function waitForReload(timeLimitMs = 5000) {
+    cy.get("#main-content section.page-leave-active", {timeout: timeLimitMs}).should("exist")
+    cy.get("#main-content section.page-enter-to").should("not.exist")
+
+    // wait for fade transition end. Without it cypress report snapshots show empty pages
+    cy.wait(500)
+}
